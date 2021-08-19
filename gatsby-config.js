3361eed6927500f663cde9c04f0a466174a869cf
Yumin Jeong <yjeong73@gatech.edu>
2021-08-19 18:21:52 -0400

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,7 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Testing Website",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: ["gatsby-plugin-gatsby-cloud",
+            "gatsby-plugin-image",
+            "gatsby-plugin-sharp",],
 };
