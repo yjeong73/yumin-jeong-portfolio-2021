d451613fcc81090404760e9769e6a1c58d67c44e
Yumin Jeong <yjeong73@gatech.edu>
2021-08-24 17:31:34 -0400

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,12 +1,25 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 
 const BlogPost = ({ data }) => {
+    const image = getImage(data.mdx.frontmatter.hero_image)
+
   return (
       <Layout pageTitle={data.mdx.frontmatter.title}>
         <p>{data.mdx.frontmatter.date}</p>
+        <GatsbyImage
+          image={image}
+          alt={data.mdx.frontmatter.hero_image_alt}
+        />
+        <p>
+        Photo Credit:{" "}
+            <Link to={data.mdx.frontmatter.hero_image_credit_link}>
+              {data.mdx.frontmatter.hero_image_credit_text}
+            </Link>
+        </p>
         <MDXRenderer>
           {data.mdx.body}
         </MDXRenderer>
@@ -15,15 +28,23 @@ const BlogPost = ({ data }) => {
 }
 
 export const query = graphql`
-  query ($id: String) {
-    mdx(id: {eq: $id}) {
-      frontmatter {
-        title
-        date(formatString: "MMMM D, YYYY")
+    query($slug: String) {
+      mdx(slug: {eq: $slug}) {
+        body
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          hero_image_alt
+          hero_image_credit_link
+          hero_image_credit_text
+          hero_image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
       }
-      body
     }
-  }
 `
 
 export default BlogPost
