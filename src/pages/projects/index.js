120cbdbe85d95abdbae959a4f8321fe020f8e98a
Yumin Jeong <yjeong73@gatech.edu>
2021-08-30 18:30:24 -0400

    dynamically added each projects to cards

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import Layout from '../../components/layout'
 import { Link, graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import {
     container,
     heading,
@@ -25,21 +26,36 @@ import {
 const ProjectPage = ({ data }) => {
   return (
     <Layout pageTitle="Projects">
+        <h1 className={heading}>Projects</h1>
         <div className={cardBody}>
-            <div className={card}>
-                <div className={cardImage}></div>
-                <div className={cardText}>
-                    <span className={date}>4 days ago</span>
-                    <h2>Post One</h2>
-                    <p> Description goes here!</p>
-                </div>
-                <div className={cardStats}>
-                    <div className={stat}>
-                        <div className={value}>4m</div>
-                        <div className={type}>read</div>
-                    </div>
-                </div>
-            </div>
+            {
+                data.allMdx.nodes.map((node) => (
+                    <article key={node.id}>
+                        <Link to={`/projects/${node.slug}`} className={projectLinks}>
+                        <div className={card}>
+                            <GatsbyImage
+                              className={cardImage}
+                              image={getImage(node.frontmatter.hero_image)}
+                            />
+                            <div className={cardText}>
+                                <span className={date}>{node.frontmatter.date}</span>
+                                <h2 className={projectTitles}>
+                                    {node.frontmatter.title}
+                                </h2>
+                                <p className={projectTechnology}>
+                                Goal&#58; {node.frontmatter.goal}</p>
+                            </div>
+                            <div className={cardStats}>
+                                <div className={stat}>
+                                    <div className={type}>Technology&#58;</div>
+                                    <div className={value}>{node.frontmatter.technology}</div>
+                                </div>
+                            </div>
+                        </div>
+                        </Link>
+                    </article>
+                    ))
+            }
         </div>
 
     </Layout>
@@ -48,17 +64,23 @@ const ProjectPage = ({ data }) => {
 
 export const query = graphql`
     query {
-      allMdx(sort: {fields: frontmatter___date, order: DESC}) {
-        nodes {
-          frontmatter {
-            date
-            title
-            technology
+        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+          nodes {
+            frontmatter {
+              date
+              title
+              technology
+              hero_image {
+                childImageSharp {
+                  gatsbyImageData
+                }
+              }
+              goal
+            }
+            id
+            slug
           }
-          id
-          slug
         }
-      }
     }
 `
 
