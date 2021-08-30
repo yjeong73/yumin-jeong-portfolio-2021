f44b047942e96d257efb925a245793870213a757
Yumin Jeong <yjeong73@gatech.edu>
2021-08-30 18:41:55 -0400

    before trying the anchor link plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import {
     homeIntro,
     homeName,
     homeDescription,
+    heading,
 } from '../components/layout.module.css'
 
 // Step 2: Define your component
@@ -17,6 +18,33 @@ const IndexPage = () => {
         <p className={homeName}>I AM YUMIN JEONG,</p>
         <p className={homeDescription}>A recent graduate with passion for computer science and art</p>
         <p className={homeDescription}>who is seeking job opportunities as a front-end engineer.</p>
+        <h1 className={heading}>About Me</h1>
+        <p className={homeDescription}>Education: Georgia Institute of Technology 2017 - 2021
+        Bachelor of Science in Computer Science Concentration: Intelligence-Media</p>
+        <p className={homeDescription}>Education: Georgia Institute of Technology 2017 - 2021
+        Bachelor of Science in Computer Science Concentration: Intelligence-Media</p>
+        <p className={homeDescription}>Education: Georgia Institute of Technology 2017 - 2021
+        Bachelor of Science in Computer Science Concentration: Intelligence-Media</p>
+        <p className={homeDescription}>Education: Georgia Institute of Technology 2017 - 2021
+        Bachelor of Science in Computer Science Concentration: Intelligence-Media</p>
+        <h2 className={heading}>Another About Me</h2>
+        <p className={homeDescription}>Education: Georgia Institute of Technology 2017 - 2021
+        Bachelor of Science in Computer Science Concentration: Intelligence-Media</p>
+        <p className={homeDescription}>Education: Georgia Institute of Technology 2017 - 2021
+        Bachelor of Science in Computer Science Concentration: Intelligence-Media</p>
+        <p className={homeDescription}>Education: Georgia Institute of Technology 2017 - 2021
+        Bachelor of Science in Computer Science Concentration: Intelligence-Media</p>
+        <p className={homeDescription}>Education: Georgia Institute of Technology 2017 - 2021
+        Bachelor of Science in Computer Science Concentration: Intelligence-Media</p>
+        <h3 className={heading}>Probably Contact</h3>
+        <p className={homeDescription}>Education: Georgia Institute of Technology 2017 - 2021
+        Bachelor of Science in Computer Science Concentration: Intelligence-Media</p>
+        <p className={homeDescription}>Education: Georgia Institute of Technology 2017 - 2021
+        Bachelor of Science in Computer Science Concentration: Intelligence-Media</p>
+        <p className={homeDescription}>Education: Georgia Institute of Technology 2017 - 2021
+        Bachelor of Science in Computer Science Concentration: Intelligence-Media</p>
+        <p className={homeDescription}>Education: Georgia Institute of Technology 2017 - 2021
+        Bachelor of Science in Computer Science Concentration: Intelligence-Media</p>
       </Layout>
   )
 }
