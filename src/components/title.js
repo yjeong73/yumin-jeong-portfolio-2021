47990b22aa10648cb6cf42ebbfecd15cdf80096d
Yumin Jeong <yjeong73@gatech.edu>
2021-09-17 22:18:43 -0400

    Moved the title section to components/js, moved the nav bar towards left

---
+++ src/components/title.js
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import { useEffect } from 'react'
+import $ from 'jquery';
+import {
+    homeIntro,
+    homeDescription,
+    introduction,
+    circle1,
+    circle2,
+    circle3,
+} from '../components/layout.module.css'
+import {
+    highlight,
+} from '../components/sections.module.css'
+
+const TitleSection = () => {
+    useEffect(() => {
+        $(document).mousemove(function (event) {
+          var windowWidth = $(window).width();
+          var windowHeight = $(window).height();
+
+          var mouseXpercentage = Math.round((event.pageX / windowWidth) * 100);
+          var mouseYpercentage = Math.round((event.pageY / windowHeight) * 100);
+
+          document.getElementById("circles").style.top = mouseXpercentage + "%";
+          document.getElementById("circles").style.left = mouseYpercentage + "%";
+        });
+    });
+
+    return (
+        <div className={introduction}>
+            <div className={circle1} id="circles"></div>
+            <div className={circle2} id="circles"></div>
+            <div className={circle3} id="circles"></div>
+
+            <div style={{width: "50%"}}>
+                <p className={homeIntro}>Hello, <br/> I am Yumin Jeong</p>
+                <p className={homeDescription}>I'm a recent graduate with a passion for
+                <br/><span className={highlight}>computer science👩‍💻</span> and <span className={highlight}>art🌻</span> who is seeking
+                <br/>job opportunities as a front-end engineer.</p>
+            </div>
+        </div>
+    )
+}
+
+export default TitleSection
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,25 @@
-// Step 1: Import React
 import * as React from 'react'
-import { useEffect } from 'react'
-//import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import TitleSection from '../components/title'
 import AboutSection from '../components/about'
 import ProjectsSection from '../components/projects'
 import ContactSection from '../components/contact'
-import $ from 'jquery';
 import {
-    homeIntro,
-    homeDescription,
-    introduction,
-    circle1,
-    circle2,
-    circle3,
     randomCircle1,
     randomCircle2,
 } from '../components/layout.module.css'
-import {
-    highlight,
-} from '../components/sections.module.css'
 
 function randomValue() {
     return Math.floor(Math.random() * 101);
 }
 
 const IndexPage = () => {
-  useEffect(() => {
-      $(document).mousemove(function (event) {
-        var windowWidth = $(window).width();
-        var windowHeight = $(window).height();
-
-        var mouseXpercentage = Math.round((event.pageX / windowWidth) * 100);
-        var mouseYpercentage = Math.round((event.pageY / windowHeight) * 100);
-
-        document.getElementById("circles").style.top = mouseXpercentage + "%";
-        document.getElementById("circles").style.left = mouseYpercentage + "%";
-      });
-  });
-
   const values = [randomValue(), randomValue(), randomValue(), randomValue(), randomValue()];
-  console.log(values);
 
   return (
       <Layout pageTitle="Home Page">
-        <div id="title" className={introduction}>
-            <div className={circle1} id="circles"></div>
-            <div className={circle2} id="circles"></div>
-            <div className={circle3} id="circles"></div>
-            <p className={homeIntro}>Hello, <br/> I am Yumin Jeong</p>
-            <p className={homeDescription}>I'm a recent graduate with a passion for
-            <br/><span className={highlight}>computer science👩‍💻</span> and <span className={highlight}>art🌻</span> who is seeking
-            <br/>job opportunities as a front-end engineer.</p>
+        <div id="title">
+            <TitleSection></TitleSection>
         </div>
 
         <div id="aboutMe" style={{position: "relative", overflow: "hidden"}}>
@@ -71,10 +39,8 @@ const IndexPage = () => {
             <div className={randomCircle1} style={{top: values[4] + "%", background: "#F2C335"}}></div>
             <div className={randomCircle2} style={{top: values[5] + "%", background: "#CDA291"}}></div>
         </div>
-
       </Layout>
   )
 }
 
-// Step 3: Export your component
 export default IndexPage
